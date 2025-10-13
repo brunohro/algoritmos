1ed92bb32e7ede7c1faa461e660e3eea7b4dad61
brunohro <brunoh.rodrigues27@gmail.com>
2025-10-13 14:08:08 -0300

    Realizando desafio do robô da disciplina de algoritmo

--- aulas/Robo/robo.c
+++ aulas/Robo/robo.c
@@ -6,53 +6,67 @@
 #define LARGURA 20
 
 void MoveEsquerda(int *x);
-// void MoveDireita(int *x);
-// void MoveCima(int *y);
-// void MoveBaixo(int *y);
-void ImprimeMapa(int px, int py);
-// void LimiteTabuleiro(int *px, int *py);
+void MoveDireita(int *x);
+void MoveCima(int *y);
+void MoveBaixo(int *y);
+void ImprimeMapa(int px, int py, char formato);
+void LimiteTabuleiro(int *px, int *py);
 int main(void)
 {
     int px, py;
     char ordem;
     int continua = 1;
+    char formato = '@';
 
     srand(time(0));
     px = 10;
     py = 10;
 
-    ImprimeMapa(px, py);
-    while (continua)
+    ImprimeMapa(px, py, formato);
+    while (continua != 0)
     {
         ordem = getchar();
-        system("cls");
+        // system("cls");
         switch (ordem)
         {
         case 'a':
         case 'A':
+            ImprimeMapa(px, py, formato);
             MoveEsquerda(&px);
-            ImprimeMapa(px, py);
             break;
         case 'd':
         case 'D':
-            // MoveDireita(&px);
-            ImprimeMapa(px, py);
+            ImprimeMapa(px, py, formato);
+            MoveDireita(&px);
             break;
         case 'w':
         case 'W':
-            // MoveCima(&py);
-            ImprimeMapa(px, py);
+            ImprimeMapa(px, py, formato);
+            MoveCima(&py);
             break;
         case 's':
         case 'S':
-            // MoveBaixo(&py);
-            ImprimeMapa(px, py);
+            ImprimeMapa(px, py, formato);
+            MoveBaixo(&py);
             break;
 
         case 'q':
         case 'Q':
             continua = 0;
             break;
+        case 'm':
+        case 'M':
+            if (formato == '@')
+            {
+                formato = '$';
+            }
+            else
+            {
+                formato = '@';
+            }
+            ImprimeMapa(px, py, formato);
+        default:
+            ImprimeMapa(px, py, formato);
         }
 
         printf("ordem %d %d \n", px, py);
@@ -61,7 +75,7 @@ int main(void)
     return 0;
 }
 
-void ImprimeMapa(int px, int py)
+void ImprimeMapa(int px, int py, char formato)
 {
     int x, y;
     for (y = 0; y < ALTURA; y++)
@@ -70,7 +84,7 @@ void ImprimeMapa(int px, int py)
         {
             if ((px == x) && (py == y))
             {
-                printf(" @ ");
+                printf(" %c ", formato);
             }
             else
             {
@@ -83,9 +97,43 @@ void ImprimeMapa(int px, int py)
 
 void MoveEsquerda(int *x)
 {
-    x++;
+    *x -= 1;
 }
 
+void MoveDireita(int *x)
+{
+    *x += 1;
+}
+
+void MoveCima(int *y)
+{
+    *y -= 1;
+}
+
+void MoveBaixo(int *y)
+{
+    *y += 1;
+}
 void LimiteTabuleiro(int *x, int *y)
 {
+    if (*x <= 0)
+    {
+        printf("\n-LIMITE DE TABOLEIRO-");
+        *x = 0;
+    }
+    if (*x >= 19)
+    {
+        printf("\n-LIMITE DE TABOLEIRO-");
+        *x = 19;
+    }
+    if (*y <= 0)
+    {
+        printf("\n-LIMITE DE TABOLEIRO-");
+        *y = 0;
+    }
+    if (*y >= 19)
+    {
+        printf("\n-LIMITE DE TABOLEIRO-");
+        *y = 19;
+    }
 }
